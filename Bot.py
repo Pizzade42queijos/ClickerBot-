660953e5de8726835f0eebbc4fe03cff17a1967e
Pzz42 <aamf@discente.ifpe.edu.br>
2024-01-05 15:13:01 -0300

    threading and mouse movement issues

--- Bot.py
+++ Bot.py
@@ -3,11 +3,10 @@ from pynput.mouse import Button, Controller
 import time
 
 class Bot:
-    def _init_(self, mouse, running, OpArea_XL, OpArea_XR, OpArea_YU, OpArea_YL):
+    def __init__(self):
         self.mouse = Controller()
         self.running = False
         self.No_limit = True #if True, program works no matter where mouse is located.
-    
         self.OpArea_XL =  0 #limit of operation area, left X axis
         self.OpArea_XR =  0 # || || right X axis
         self.OpArea_YU =  0 # || || upper Y axis
@@ -18,31 +17,34 @@ class Bot:
         #get mouse position, returns true if in set operation square.
 
     def Run(self):
+        
         if self.No_limit == False:
             if Is_in_OpSquare():
                 self.running = True
-                Clicker(running)
+                self.Clicker(True)
             else:
                 self.running = False
-                Clicker(running)
+                Clicker(False)
         elif self.running:
             self.running = False
-            Clicker(running)
+            self.Clicker(False)
         else:
             self.running = True
-            Clicker(running)
+            self.Clicker(True)
 
     def Stop_bot(self):
         self.running = False
         return self.running
-
-    def Clicker(turner):
+    
+    def Clicker(self, turner):
         while turner:
-            mouse.click(Button.left, 1)
+            self.mouse.click(Button.left, 1)
             time.sleep(0.001)
             
     def Limiter_mode():
         return 0
+
+bot_instance = Bot()
     
 class Imputer:
     def press_on(key):
@@ -51,8 +53,8 @@ class Imputer:
         print('Press OFF: {}'.format(key))
         if key == Key.esc:
             print('Script Finalized')
-            return Bot.Stop_bot()
+            return bot_instance.Stop_bot()
         elif key.char == ('s'):
-            Bot.Run()
+            bot_instance.Run()
     with Listener(on_press = press_on, on_release = press_off) as listener:
         listener.join()
